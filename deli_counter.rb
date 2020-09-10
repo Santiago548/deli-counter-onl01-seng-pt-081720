e7ea2f25e562c8e1ba9e6cb2fcc1a05674919ae1
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-10 17:40:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,14 @@
 # Write your code here.
-def line(katz_deli)
-  if katz_deli.empty?
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |person, index|
+    deli.each.with_index(1) do |person, index|
       current_line << " #{index}. #{person}"
     end
     puts current_line
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
 
-end
